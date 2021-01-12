f583faf7ac72aae0a885783de0ac3336461b7f01
smrza <69464039+smrza@users.noreply.github.com>
2021-01-12 12:10:20 +0100

    Add files via upload

---
+++ src/app/models/history-record.model.ts
@@ -0,0 +1,11 @@
+export class HistoryRecord 
+{
+    inputHistory: String
+    outputHistory: String
+
+    constructor(inputHistory: String, outputHistory: String) 
+    {
+        this.inputHistory = inputHistory;
+        this.outputHistory = outputHistory;
+    }
+}
